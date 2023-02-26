653923dad713767d108f4e841e162412d9f1779c
Grazi Firmino <grazi.firmino.15@gmail.com>
2023-02-26 18:29:03 -0300

    Rotas funcionando
    Até o momento as rotas parecem funcionar.

--- server/index.js
+++ server/index.js
@@ -10,9 +10,24 @@ app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 
 // adicionando rotas para GET, PUT e DELETE com base no router de tasks
-app.get('/api/tasks/:id', tasksRouter);
-app.put('/api/tasks/:id', tasksRouter);
-app.delete('/api/tasks/:id', tasksRouter);
+app.get('/api/tasks/:id', (req, res) => {
+    // manipulador de rota para a rota GET /api/tasks/:id
+    // chama o manipulador de rota correspondente no router de tasks
+    tasksRouter.get(req, res);
+  });
+  
+  app.put('/api/tasks/:id', (req, res) => {
+    // manipulador de rota para a rota PUT /api/tasks/:id
+    // chama o manipulador de rota correspondente no router de tasks
+    tasksRouter.put(req, res);
+  });
+  
+  app.delete('/api/tasks/:id', (req, res) => {
+    // manipulador de rota para a rota DELETE /api/tasks/:id
+    // chama o manipulador de rota correspondente no router de tasks
+    tasksRouter.delete(req, res);
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
