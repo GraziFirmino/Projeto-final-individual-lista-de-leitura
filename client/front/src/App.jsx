e9bd085f4e1ef4512772d6ee9f8480a3b93011b0
Grazi Firmino <grazi.firmino.15@gmail.com>
2023-02-26 23:11:00 -0300

    tentando implementar o frontend

---
+++ client/front/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default App
+
